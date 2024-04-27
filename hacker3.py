ddf0e09b5ce1719a762fbbfc557863a9635d2620
rachanaruthu <167548640+rachanaruthu@users.noreply.github.com>
2024-04-27 11:15:34 +0530

    Create hacker3.py

---
+++ hacker3.py
@@ -0,0 +1,7 @@
+The provided code stub reads two integers,  and , from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
